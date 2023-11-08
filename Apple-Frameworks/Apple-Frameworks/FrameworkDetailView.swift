3a9c6264267af8ae20a77451634b2637dd444aec
Turner Naef <Turner.Naef@gmail.com>
2023-11-08 14:11:34 -0700

    framework detail and modal

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkDetailView.swift
@@ -0,0 +1,52 @@
+//
+//  FrameworkDetailView.swift
+//  Apple-Frameworks
+//
+//  Created by Turner Naef on 11/8/23.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    
+    var framework: Framework
+    var body: some View {
+        VStack {
+            HStack {
+                Spacer()
+                
+                Button {
+                    
+                } label: {
+                    Image(systemName: "xmark")
+                    // .label is system call for opposite of the system color (dark mode = white and vice versa)
+                        .foregroundColor(Color(.label))
+                        .imageScale(.large)
+                        .frame(width: 44, height: 44)
+                }
+            }
+            .padding()
+            
+            Spacer()
+            FrameworkTitleView(framework: framework)
+            Text(framework.description)
+                .font(.body)
+                .padding()
+            
+            Spacer()
+            
+            Button {
+                
+            } label: {
+                AFButton(title: "Learn More")
+            }
+        }
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView(framework: MockData.sampleFramework)
+    }
+}
+
