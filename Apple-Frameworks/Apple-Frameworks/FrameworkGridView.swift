d234d1fed61b12a65538f224b1e3c917adb2de6d
Turner Naef <Turner.Naef@gmail.com>
2023-11-08 11:35:43 -0700

    project setup + images/content

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Turner Naef on 11/8/23.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
