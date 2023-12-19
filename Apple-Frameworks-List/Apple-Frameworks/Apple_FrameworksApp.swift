f58db902865251b3c46f8d11e9631294bfa1e10e
Turner Naef <Turner.Naef@gmail.com>
2023-12-19 18:11:52 -0500

    List view of apple frameworks

---
+++ Apple-Frameworks-List/Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,23 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Turner Naef on 11/8/23.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            FrameworkGridView()
+        }
+    }
+}
+
+//struct Apple_FrameworkApp: PreviewProvider {
+//    static var previews: some View {
+//        Apple_FrameworksApp()
+//    }
+//}
---
+++ Apple-Frameworks-List/Apple-Frameworks/Screens/FrameworkGridView/FrameworkGridView.swift
@@ -0,0 +1,33 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Turner Naef on 11/8/23.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    
+    @StateObject var viewModel = FrameworkGridViewModel()
+    
+    var body: some View {
+        NavigationView {
+            List {
+                ForEach(MockData.frameworks) { framework in
+                    NavigationLink(destination: FrameworkDetailView(framework: framework, isShowingDetailView: $viewModel.isShowingDetailView)) {
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("🍎 Frameworks")
+        }
+        .accentColor(Color(.label))
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
