d1d30d810a52368e43702f1b988bccd0b139cd5f
Turner Naef <Turner.Naef@gmail.com>
2023-12-22 00:21:15 -0500

    Loading indicator when fetching from server

---
+++ Appetizers/Appetizers/Color+Ext.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Ext.swift
+//  Appetizers
+//
+//  Created by Turner Naef on 12/22/23.
+//
+
+import SwiftUI
+
+extension Color {
+    static let brandPrimary = Color("brandPrimary")
+}
+
+extension UIColor {
+    static let brandPrimary = UIColor(named: "brandPrimary")
+}
